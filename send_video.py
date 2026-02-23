dd0a3c986bbe16a2ad010210a4c9ec8cabcc9cf2
beaglbone <85814777+beaglbone@users.noreply.github.com>
2026-02-23 20:31:17 +0530

    Update send_video.py

--- send_video.py
+++ send_video.py
@@ -1,29 +1,3 @@
-# import os
-# from telethon import TelegramClient
-
-# api_id = int(os.environ["API_ID"])
-# api_hash = os.environ["API_HASH"]
-# session = os.environ["SESSION"]
-# chat_id = int(os.environ["CHAT_ID"])
-
-# VIDEO_DIR = "videos"
-
-# client = TelegramClient(session, api_id, api_hash)
-
-# async def main():
-#     await client.start()
-
-#     for file in os.listdir(VIDEO_DIR):
-#         if file.endswith(".mp4"):
-#             path = os.path.join(VIDEO_DIR, file)
-#             print("Sending:", path)
-#             await client.send_file(
-#                 chat_id,
-#                 path,
-#                 supports_streaming=True
-#             )
-
-
 import os
 from telethon import TelegramClient
 from telethon.sessions import StringSession
@@ -67,7 +41,3 @@ with client:
 
     
 
-    await client.disconnect()
-
-with client:
-    client.loop.run_until_complete(main())
